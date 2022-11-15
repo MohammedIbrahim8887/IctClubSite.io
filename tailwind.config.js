e9d714b182d4c45ed7498d29dbe2b13446f26790
MohammedIbrahim8887 <95257247+MohammedIbrahim8887@users.noreply.github.com>
2022-11-15 19:52:38 +0300

    Initial Push

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+        screens: {
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
+        },
+        extend: {
+            colors: {
+                DarkBlue: "#2F2E41",
+                SunsetYellow: "#F9A826",
+            },
+        },
+        fontFamily: {
+            sans: "Poppins,Inter,Helvetica, Arial, sans-serif",
+        },
+        fontsize: {
+            xsm: "0.4rem"
+        },
+    },
+    plugins: [],
+};
